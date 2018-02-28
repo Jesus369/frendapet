1d6ff589ed43bc89cf2b4209d81ae324753d9cb2
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-28 14:51:23 -0600

    Prepping Homepage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 
 /* Components */
 import { Header } from "../components/Header";
+import HomeRegister from "../components/HomeRegister";
+import { SectionOne } from "../components/SectionOne";
 
 export class Home extends Component {
   render() {
     return (
       <div>
         <Header />
+        <HomeRegister />
+        <SectionOne />
       </div>
     );
   }
