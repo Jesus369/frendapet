59117e15951553317b87574f668cbbfc178cabdf
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-28 14:48:38 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
+
+/* Styles */
 import "./styles/Styles.css";
+import "./styles/Mobile.css";
 import "./styles/Desktop.css";
+import "./styles/Monitor.css";
 
 import Routes from "./routes";
 
-const App = <Routes />;
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import { setContext } from "apollo-link-context";
+import { ApolloLink } from "apollo-link";
+
+const httpLink = createHttpLink({ uri: "http://localhost:8081/graphql" });
+
+const middlewareLink = setContext(() => ({
+  headers: {
+    "x-token": localStorage.getItem("token"),
+    "x-refresh-token": localStorage.getItem("refreshToken")
+  }
+}));
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  const { headers } = operation.getContext();
+
+  if (headers) {
+    const token = headers.get("x-token");
+    const refreshToken = headers.get("x-refresh-token");
+
+    if (token) {
+      localStorage.setItem("token", token);
+    }
+
+    if (refreshToken) {
+      localStorage.setItem("refreshToken", refreshToken);
+    }
+  }
+
+  return forward(operation);
+});
+
+const link = afterwareLink.concat(middlewareLink.concat(httpLink));
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
 
 ReactDOM.render(App, document.getElementById("root"));
 registerServiceWorker();
