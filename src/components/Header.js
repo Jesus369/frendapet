65406e49722503d4334e47055544ac7269d74cfc
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-28 14:50:07 -0600

    NavBar Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,14 @@ export class Header extends Component {
   render() {
     return (
       <div className="navbar">
-        <table>
-          <tr>
-            <td>Jesus</td>
-          </tr>
+        <table className="enter_site">
+          <tbody>
+            <tr>
+              <td className="enter_text">Signin</td>
+              <td>/</td>
+              <td className="enter_text">Signup</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
