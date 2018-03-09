84bd9ea83321b1f3435121810cd5e23c12fe3811
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-03-09 10:05:58 -0600

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { Home } from "./Home";
+import Home from "./HomePage/Home";
 import AllUsers from "./AllUsers";
-import Login from "./Login";
 import Register from "./Register";
-import WalkerRegister from "./WalkerRegister";
+import WalkerRegister from "./WalkerRegister/WalkerRegister";
 import SearchWalker from "./SearchWalker";
 import AddPet from "./AddPet";
-import UserRoute from "./UserRoute";
+import UserRoute from "./userpage/UserRoute";
+import WalkerRoute from "./WalkerRoute";
 
 export default () =>
   <BrowserRouter>
     <Switch>
+      <Route path="/walker/:walkerid" exact component={WalkerRoute} />
       <Route path="/user/:id" exact component={UserRoute} />
       <Route path="/add-pet" exact component={AddPet} />
       <Route path="/search-walker" exact component={SearchWalker} />
       <Route path="/dogwalker-register" exact component={WalkerRegister} />
       <Route path="/register" exact component={Register} />
-      <Route path="/login" exact component={Login} />
       <Route path="/users" exact component={AllUsers} />
       <Route path="/" exact component={Home} />
     </Switch>
