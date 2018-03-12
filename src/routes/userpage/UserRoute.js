01564748aeb85ee49e00c0f846aa72d703df2512
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-03-12 11:03:22 -0500

    Update UserRoute.js

--- src/routes/userpage/UserRoute.js
+++ src/routes/userpage/UserRoute.js
@@ -1,28 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import decode from "jwt-decode";
 import PropTypes from "prop-types";
 
+import Center from "./components/Center";
+import LeftTab from "./components/LeftTab";
 import RightTab from "./components/RightTab";
-import BottomTab from "./components/BottomTab";
 
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
 
 const UserRoute = ({ user }) => {
-  console.log(user.firstname);
   let userId = "";
-  let email = "";
   try {
     const token = localStorage.getItem("token");
     const { user } = decode(token);
     userId = parseInt(user.id, 10);
-    email = user.email;
   } catch (err) {}
   return (
-    <div>
-      {user.firstname}
-      <RightTab email={email} userId={userId} />
-      <BottomTab owner={userId} pets={user.pets} />
+    <div className="userpage_contains">
+      <LeftTab userId={userId} />
+      <Center pets={user.pets} userId={user.id} />
+      <RightTab owner={userId} pets={user.pets} />
     </div>
   );
 };
@@ -44,6 +42,7 @@ const getUserQuery = gql`
       pets {
         id
         name
+        image
       }
     }
   }
